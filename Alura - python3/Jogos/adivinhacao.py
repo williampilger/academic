2f4a5c1862d5f6f93705823a881e9d537c0b4a51
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-03-06 23:14:48 -0300

    Update adivinhacao.py

--- Alura - python3/Jogos/adivinhacao.py
+++ Alura - python3/Jogos/adivinhacao.py
@@ -1,8 +1,21 @@
+import random
 print("Este é um jogo de adivinhação!")
 
-numero = 56
-tentativas = 3
+numero = random.rangrange(1,101)
 
+nivel = 0
+print("(1) Fácil\n(2) Médio\n(3) Difícil")
+while(nivel < 1 or nivel > 3):
+    nivel = input("Escolha seu nível")
+if(nivel == 1):
+    tentativas = 20
+elif(nivel == 2):
+    tentativa = 10
+else:
+    tentativas = 5
+
+pontos = 1000
+    
 for rodada in range(1, tentativas + 1):
     if(rodada == 1):
         print("Faça sua aposta", end="")
@@ -36,6 +49,7 @@ for rodada in range(1, tentativas + 1):
         else:
             print("Eh, infelizmente não foi dessa vez.\nO número secreto era", numero)
         erro_anterior = erro
+    pontos -= erro_anterior #erro_anterior sendo usado por que se ele chutar um número fora do intervalo só o erro_anterior é definido
 
 
 
