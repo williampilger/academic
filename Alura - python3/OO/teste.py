e1ad26bfe5098c0e758d80ba31498311cc72ce0e
William Pilger <pilger.will@gmail.com>
2021-03-11 12:55:21 -0300

    .

--- Alura - python3/OO/teste.py
+++ Alura - python3/OO/teste.py
@@ -1,5 +1,13 @@
 def cria_conta(numero, titular, saldo, limite):
-    conta = ["numero": numero, "titular": titular, "saldo": saldo, "limite", limite]
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite", limite}
     return conta
 
-def
+def deposita(conta, valor):
+    conta["valor"] += valor
+
+def saca(conta, valor):
+    conta["valor"] -= valor
+
+def extrato(conta):
+    print(conta["valor"])
+
