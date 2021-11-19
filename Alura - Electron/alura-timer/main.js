7dc397f6109ce92d368b88e51d246a6bb36bcb25
williamauthenty <pilger.will@gmail.com>
2021-11-19 10:58:54 -0300

    Upld

--- Alura - Electron/alura-timer/main.js
+++ Alura - Electron/alura-timer/main.js
@@ -1,1 +1,10 @@
-console.log("Olá Mundo!!");
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+    console.log("Aplicação Iniciada.");
+    let mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+    mainWindow.loadURL('https://authenty.com.br/v1');
+});
