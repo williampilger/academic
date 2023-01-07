11f452d97b23494783945a364455e80072b8dd48
williampilger <pilger.will@gmail.com>
2023-01-07 18:31:56 -0300

    upld

---
+++ RocketSeat-Ignite_Lab-NodeJS/notifications-service/src/app/use-cases/cancel-notification.ts
@@ -0,0 +1,34 @@
+/**
+ * Esta classe é responsável por CANCELAR o envio de uma notificação.
+ */
+
+import { Injectable } from "@nestjs/common";
+import { NotificationsRepository } from "../repositories/notifications-repository";
+
+interface CancelNotificationRequest {
+    notificationId: string,
+}
+
+type CancelNotificationResponse = void;
+
+@Injectable()
+export class CancelNotification {
+
+    constructor(private notificationsRepository: NotificationsRepository){}
+
+    async execute(request: CancelNotificationRequest):Promise<CancelNotificationResponse> {
+        const { notificationId } = request
+
+
+
+        const notification = await this.notificationsRepository.findById(notificationId);
+        
+        if(!notification){
+            throw new Error('2301071809 - Impossible get this notification.');
+        }
+
+        await this.notificationsRepository.save(notification);
+
+        return;
+    }
+}
