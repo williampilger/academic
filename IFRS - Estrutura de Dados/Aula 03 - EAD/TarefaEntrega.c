b4023063c83496fc614cc670319a17e402b9d0df
edu-pilger-will <129803121+edu-pilger-will@users.noreply.github.com>
2025-08-18 22:09:36 -0300

    Update TarefaEntrega.c

--- IFRS - Estrutura de Dados/Aula 03 - EAD/TarefaEntrega.c
+++ IFRS - Estrutura de Dados/Aula 03 - EAD/TarefaEntrega.c
@@ -3,62 +3,63 @@
  */
 
 #include <stdio.h>
-#define TAM 5// número de pessoas
+#define TAM 20// número de pessoas
 
 int main(void) {
 
     int cadastro[TAM]={0};
     int option=0, valor=0;
-    int i;
+    int i,j;
 
     do {
 
-        printf(" ----------------------------\n");
+        printf("\n\n ----------------------------\n  ");
         
         for(i=0;i<TAM;i++){
             printf("[%d] ", cadastro[i]);
         }
         
         printf("\n ----------------------------\n");
-        printf(" 0 - Sair do Software\n");
-        printf(" 1 - Add Número\n");
-        printf(" 2 - Remover como PILHA\n");
-        printf(" 3 - Remover como FILA\n");
+        printf("  0 - Sair do Software\n");
+        printf("  1 - Add Número%s\n", cadastro[TAM-1]==0 ? "" : "     >> NÃO HÁ ESPAÇO!");
+        printf("  2 - Remover como PILHA%s\n", cadastro[0]==0 ? "     >> NÃO HÁ DADOS PARA EXCLUIR" : "");
+        printf("  3 - Remover como FILA%s\n", cadastro[0]==0 ? "     >> NÃO HÁ DADOS PARA EXCLUIR" : "");
         printf(" ----------------------------\n");
         scanf("%d", &option);
 
         switch(option){
             case 1://add
+                if ( ! cadastro[TAM-1]==0 ) break;
                 printf("\n Digite o valor:");
                 scanf("%d", &valor);
                 if(valor == 0){
-                    for(int j=0;j<TAM;j++){
+                    printf("\n >> Não pode ser zero!");
+                } else {
+                    for(j=0;j<TAM;j++){
                         if(cadastro[j]==0){
                             cadastro[j] = valor;
                             break;
                         }
                     }
-                } else {
-                    printf("\n >> Não pode ser zero!");
                 }
                 break;
-            case 1://PILHA
-                for(int j=TAM-1; j>=0; j--){
+            case 2://PILHA
+                for(j=TAM-1; j>=0; j--){
                     if(cadastro[j] > 0){
                         cadastro[j] = 0;
                         break;
                     }
                 }
                 break;
-            case 1://FILA
-                for(int j=0; j<TAM - 1;j++){
+            case 3://FILA
+                for(j=0; j<TAM - 1;j++){
                     cadastro[j] = cadastro[j+1];
                 }
                 cadastro[TAM-1] = 0;
                 break;
         }
 
-    } while(option != 0)
+    } while(option != 0);
 
     printf(" >> Software Encerrado! ");
 
