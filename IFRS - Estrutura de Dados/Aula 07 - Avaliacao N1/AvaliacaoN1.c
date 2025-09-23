48cb72789d7088341a2f4195c73e7cf26a4f5039
edu-pilger-will <129803121+edu-pilger-will@users.noreply.github.com>
2025-09-23 00:29:55 +0000

    prova AV1

---
+++ IFRS - Estrutura de Dados/Aula 07 - Avaliacao N1/AvaliacaoN1.c
@@ -0,0 +1,70 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TAM 2
+
+struct Estudante {
+    char nome[50];
+    float N1,N2,media;
+};
+
+int main(void){
+
+    struct Estudante estudantes[TAM];
+
+
+    for(int i=0; i < TAM; i++){
+        printf(" Digite o nome do estudante %d: ", i+1);
+        scanf(" %[^\n]", estudantes[i].nome);
+        
+        printf(" Informe a N1 do estudante %d (0-10): ", i+1);
+        scanf("%f", &estudantes[i].N1);
+        
+        printf(" Informe a N2 do estudante %d (0-10): ", i+1);
+        scanf("%f", &estudantes[i].N2);
+
+        //Poder-se-ia calcular a média imediatamente aqui, mas, como o problema pede pra calcular depois, vou fazer mais um laço, só pra garantir
+    }
+
+    for(int i=0; i<TAM; i++){
+        estudantes[i].media = (estudantes[i].N1 + estudantes[i].N2) / 2;
+        //Poder-se-ia calcular a média geral aqui também, mas, pra sermos literais...
+    }
+
+    float med = 0;
+    for(int i=0;i<TAM;i++){
+        med += estudantes[i].media;
+    }
+    med /= TAM;
+
+    //Ordenação
+    bool mudou;
+    struct Estudante temp;
+    do {
+        mudou = false;
+        for(int i=0;i<TAM-1;i++){
+            if( estudantes[i].media < estudantes[i+1].media ){
+                mudou = true;
+                temp = estudantes[i];
+                estudantes[i] = estudantes[i+1];
+                estudantes[i+1] = temp;
+            }
+        }
+    } while (mudou);
+    
+    //Exibição
+    printf("\n +----------------------------------------------------+------+------+------+");
+    printf("\n |                                               Nome |  N1  |  N2  | MED. |");
+    printf("\n +----------------------------------------------------+------+------+------+");
+    for(int i=0;i<TAM;i++){
+        printf("\n | %50s | %.2f | %.2f | %.2f |", estudantes[i].nome, estudantes[i].N1, estudantes[i].N2, estudantes[i].media);
+        printf("\n +----------------------------------------------------+------+------+------+");
+    }
+    printf("\n | A média geral da turma é de  %2.2f pontos.                               |", med);
+    printf("\n +-------------------------------------------------------------------------+");
+    
+    printf("\n\n\n Fim do programa!");
+
+}
